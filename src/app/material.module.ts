618c6f36f03820417aaa01c8e0b4755e7c31e7b4
crudaz <romerocruz@gmail.com>
2020-06-27 17:18:12 -0500

    feat: angular material implementation

---
+++ src/app/material.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+
+// Modules
+import { MatButtonModule } from "@angular/material/button";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatIconModule } from "@angular/material/icon";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatToolbarModule } from "@angular/material/toolbar";
+
+const MaterialComponents = [
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule,
+];
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents],
+})
+export class MaterialModule {}
