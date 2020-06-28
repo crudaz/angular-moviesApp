af3ab9208aab01fb7b7b7360b4a2ed63751cda87
crudaz <romerocruz@gmail.com>
2020-06-28 11:47:32 -0500

    feat: adding routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+import { HomeComponent } from "./home/home.component";
+import { MovieComponent } from "./movie/movie.component";
+import { TopComponent } from "./top/top.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "add-movie", component: MovieComponent },
+  { path: "top5", component: TopComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
