ef2ad59f2e1142704278bdfa8e90973bde24ec47
crudaz <romerocruz@gmail.com>
2020-06-29 23:08:15 -0500

    feat: adding component on app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,41 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-
 import { MaterialModule } from "./material.module";
-import { HomeComponent } from './home/home.component';
-import { MovieComponent } from './movie/movie.component';
-import { TopComponent } from './top/top.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { MovieItemComponent } from './movie-item/movie-item.component';
-import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+
+// Components
+import { HomeComponent } from "./components/home/home.component";
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
+import { MovieAddComponent } from "./components/movie-add/movie-add.component";
+import { MovieDetailComponent } from "./components/movie-detail/movie-detail.component";
+import { MovieItemComponent } from "./components/movie-item/movie-item.component";
+import { TopComponent } from "./components/top/top.component";
+import { MovieService } from "./services/movie.service";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, MovieComponent, TopComponent, NavBarComponent, MovieItemComponent, MovieDetailComponent],
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    MovieAddComponent,
+    MovieDetailComponent,
+    MovieItemComponent,
+    TopComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
     MaterialModule,
   ],
-  providers: [],
+  providers: [MovieService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
